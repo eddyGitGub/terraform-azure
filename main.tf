521a557b56f614a47d9cea8d2c59e0fd63ef0140
Edward Aleonope <eddycase1@gmail.com>
2022-07-05 10:44:08 +0100

    adding vm
    adding vm

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rgdev" {
   name     = "rgdev-resources"
-  location = "westus2"
+  location = "eastus"
 
   tags = {
     Environment = "dev"
@@ -31,5 +31,123 @@ resource "azurerm_virtual_network" "rgdev-vn" {
 
   tags = {
     Environment = "dev"
+  }
+}
+
+resource "azurerm_subnet" "rgdev-subnet" {
+  name                 = "rgdev-subnet"
+  resource_group_name  = azurerm_resource_group.rgdev.name
+  virtual_network_name = azurerm_virtual_network.rgdev-vn.name
+  address_prefixes     = ["10.123.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "rgdev-sg" {
+  name                = "rgdev-sg"
+  location            = azurerm_resource_group.rgdev.location
+  resource_group_name = azurerm_resource_group.rgdev.name
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "rgdev-dev-rule" {
+
+  name                        = "rgdev-dev-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rgdev.name
+  network_security_group_name = azurerm_network_security_group.rgdev-sg.name
+
+}
+
+resource "azurerm_subnet_network_security_group_association" "rgdev-sga" {
+  subnet_id                 = azurerm_subnet.rgdev-subnet.id
+  network_security_group_id = azurerm_network_security_group.rgdev-sg.id
+}
+
+resource "azurerm_public_ip" "rgdev-ip" {
+  name                = "rgdev-ip"
+  resource_group_name = azurerm_resource_group.rgdev.name
+  location            = azurerm_resource_group.rgdev.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+resource "azurerm_network_interface" "rgdev-nic" {
+  name                = "rgdev-nic"
+  location            = azurerm_resource_group.rgdev.location
+  resource_group_name = azurerm_resource_group.rgdev.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.rgdev-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.rgdev-ip.id
+
+  }
+  tags = {
+    Environment = "dev"
+  }
+}
+
+# resource "azurerm_windows_virtual_machine" "rgdev-vm" {
+#   name                = "rgdev-vm"
+#   resource_group_name = azurerm_resource_group.rgdev.name
+#   location            = azurerm_resource_group.rgdev.location
+#   size                = "Standard_F1"
+#   admin_username      = "adminuser"
+#   admin_password      = "P@$$w0rd1234!"
+#   network_interface_ids = [
+#     azurerm_network_interface.rgdev-nic.id,
+#   ]
+
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+
+#   source_image_reference {
+#     publisher = "MicrosoftWindowsServer"
+#     offer     = "WindowsServer"
+#     sku       = "2016-Datacenter"
+#     version   = "latest"
+#   }
+# }
+
+resource "azurerm_linux_virtual_machine" "rgdev-vm" {
+  name                = "rgdev-vm"
+  resource_group_name = azurerm_resource_group.rgdev.name
+  location            = azurerm_resource_group.rgdev.location
+  size                = "Standard_DS1_v2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.rgdev-nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/rgdevazurekey.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
 }
